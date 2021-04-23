c0079415fe96bdf7c7f2cd432c3d828b3a05fe47
sonali <sllenadu@uark.edu>
2021-04-23 00:34:40 -0500

    committing changes made to PARSEGFF.py for assignment 7 question 2

--- parseGFF.py
+++ parseGFF.py
@@ -6,10 +6,12 @@ import csv
 #importing argparse packge
 import argparse
 from Bio import SeqIO # importing SeqIO (is a command) in biopython 
+from collections import defaultdict
+import re
 
 # declaring rev_comp function
 def rev_comp(sequence):
-    print(sequence.reverse_complement())
+    return(sequence.reverse_complement())
 
 
 # this script will parse a GFF file and extract each feature from the genome
@@ -49,29 +51,49 @@ genome = SeqIO.read(args.fasta, 'fasta')
 #print(genome.id)
 #print(len(genome.seq))
 
+# creating a dictionary for header
+thisdict = defaultdict(dict)
+        
+# building the coding sequence (CDS) 
+with open(args.gff, 'r') as gff_in: # reads the GFF file
+    reader = csv.reader(gff_in, delimiter='\t') 
+    for line in reader:
+        if line[2] == "CDS":
+            start = int(line[3])-1 # use column 3, python indexing considers the first integer as 0 therefore, substract by 1
+            end = int(line[4])
+            strand = line[6]
+            desc = line[-1]
+            re_exon = re.search("exon (\d)", desc)
+        # creating the header
+            name = line[-1]
+            split = name.split()[1]
+            exon_num = name.split()[3]
+            species = line[0]
+            split_species = species.split()
+            #print(">" + split_species[0] + "_" + split_species[1] + "_" +  split)
+            header = str(">" + split_species[0] + "_" + split_species[1] + "_" +  split)
+        
+            if strand =="-":
+                seq = rev_comp(genome.seq[start:end])
+            else:
+                seq = genome.seq[start:end]
+            # adding value to dictionary key
+            if not re_exon:
+                thisdict[header]= seq
+            elif not thisdict[header]:
+                thisdict[header] = ['']*10
+                thisdict[header][int(exon_num)-1] = seq
+            else:
+                thisdict[header][int(exon_num)-1] = seq
+            
 
-# create csv reader object 
-with open(args.gff, 'r') as gff_in:
-    reader = csv.reader(gff_in, delimiter='\t')
-    for line in reader: # loop over all lines in the reader object ie; parsed file
-        # for CSV files using (',') is insufficient. You can use athe csv package for this
-        start = int(line[3])-1 # use column 3, python indexing considers the first integer as 0 therefore, substract by 1
-        end = int(line[4])
-        strand = line[6]
-        desc = line[-1]
-        
-        
-        # extract the sequence
-        print(">"+genome.id , desc)
-        if strand =="-":
-            rev_comp(genome.seq[start:end])
-        else:
-            print(genome.seq[start:end])
-        print()
-        
-        
+for l in thisdict:
+    thisdict[l] = ''.join(str(exon) for exon in thisdict[l])
 
-# parse the GFF file
+for i,j in thisdict.items():
+    print(i)
+    print(j)            
+            
 
 
 
